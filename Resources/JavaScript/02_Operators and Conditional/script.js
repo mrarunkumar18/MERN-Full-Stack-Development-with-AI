5146e0b43c90baa86e0423cdb8df135cb76359da
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-04-28 23:13:19 +0530

    feat: add arithmetic and assignment operator examples and documentation

---
+++ Resources/JavaScript/02_Operators and Conditional/script.js
@@ -0,0 +1,58 @@
+
+
+// This will print "Hello World" in the console.
+console.log("Hello World");
+
+
+// Arithmetic Operators:
+// let a = 5;
+// let b = 8;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+
+//unary Operator 
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a, "b =", b);
+
+// a++;
+// a--;
+
+// console.log("++a =", ++a); // Pre Increment
+// console.log("--a =", --a); // Pre Decrement
+
+// console.log("a++ =", a++); // Post Increment
+// console.log("a-- =", a--); // Post Decrement
+
+
+//Assignment Operator
+
+
+let a = 5;
+let b = 2;
+
+a += 4; // a = a + 4
+console.log("a = ", a); // 9
+
+a -= 4; // a = a - 4
+console.log("a = ", a); // 5
+
+a *= 4; // a = a * 4
+console.log("a = ", a); // 20
+
+a /= 4; // a = a / 4
+console.log("a = ", a); // 5
+
+a %= 4; // a = a % 4
+console.log("a = ", a); // 1
+
+a **= 4; // a = a ** 4
+console.log("a = ", a); // 625
