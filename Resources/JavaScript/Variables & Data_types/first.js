47658a29f950381ac8abb3320282fb631fdf0e9d
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-04-28 17:55:18 +0530

    feat: add JavaScript basics script and explanatory notes on variables and data types

--- Resources/JavaScript/Variables & Data_types/first.js
+++ Resources/JavaScript/Variables & Data_types/first.js
@@ -1,3 +1,37 @@
-console.log("Hello Arun");
-console.log("Welcome to JavaScript");
-console.log("i love coding");
+// console.log("Hello Arun");
+// console.log("Welcome to JavaScript");
+// console.log("i love coding");
+
+// fullName = "Black Panther";
+// age = 23;
+// price = 1099.9;
+// x = null;
+// isfollow = true;
+// y = false;
+
+// fullName = "Arun";
+// age = 23;
+
+// console.log(price);
+// console.log(fullName);
+// console.log(age);
+// console.log(x);
+// console.log(isfollow);
+
+//  ** 
+
+ 
+// fullname = "Black Panther";
+// FullName = "Arun Singh";
+// console.log(fullname);
+// console.log(FullName);
+
+
+// **
+
+let name = "Arun";
+let age = 23;
+let price = 100.9;
+
+
+console.log(name);
