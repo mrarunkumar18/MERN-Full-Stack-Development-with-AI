a60ea28b3316a1e176eff77831e6c908f92020e7
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-04-28 22:44:05 +0530

    refactor: reorganize JavaScript resources into structured directories and add new operators notes

---
+++ Resources/JavaScript/01_Variables & Data_types/PQ2.js
@@ -0,0 +1,17 @@
+// Qs2: Create a const object called "profile" to store the date of any instagram user.
+// name, username, age, isFollow, post, followers, following  
+
+
+const profile = {
+    fullName : "Arun Singh",
+    username : "@itz_arun_1806",
+    age : 23,
+    isFollow : true,
+    posts : 125,
+    followers : 10000,
+    following : 500,
+}
+
+console.log(profile);
+
+
--- Resources/JavaScript/Variables & Data_types/PQ2.js
+++
@@ -1,3 +0,0 @@
-// Qs2: Create a const object called "profile" to store the date of any instagram user.
-// name, username, age, isFollow, post, followers, following  
-
