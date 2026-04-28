2f89c4cd8231bcb0a97a3edbc1a1943c17f26346
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-04-28 18:55:22 +0530

    feat: add practice questions regarding JavaScript object creation

---
+++ Resources/JavaScript/Variables & Data_types/PQ1.js
@@ -0,0 +1,15 @@
+//Qs1: Create a const object called "product" to store the following data: 
+//name, price, rating, stock (100)
+
+ const product = {
+    name : "Pen",
+    price : 150,
+    rating : 4.5,
+    stock : 100,
+
+ };
+
+ console.log(product);
+
+
+ 
---
+++ Resources/JavaScript/Variables & Data_types/PQ2.js
@@ -0,0 +1,3 @@
+// Qs2: Create a const object called "profile" to store the date of any instagram user.
+// name, username, age, isFollow, post, followers, following  
+
