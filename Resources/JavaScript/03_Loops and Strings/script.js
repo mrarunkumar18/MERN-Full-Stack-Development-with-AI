4c09a30e6fee9629ac3d4b912fbedf276c6bbc8b
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-04-30 17:03:23 +0530

    feat: add VS Code launch configurations and JavaScript loops study materials

--- Resources/JavaScript/03_Loops and Strings/script.js
+++ Resources/JavaScript/03_Loops and Strings/script.js
@@ -1,5 +1,37 @@
-console.log("Hello, Arun!");
-console.log("Hello, Arun!");
-console.log("Hello, Arun!");
-console.log("Hello, Arun!");
-console.log("Hello, Arun!");
+// console.log("Hello, Arun!");
+// console.log("Hello, Arun!");
+// console.log("Hello, Arun!");
+// console.log("Hello, Arun!");
+// console.log("Hello, Arun!");
+
+//For Loop 
+
+// Print 1 to 100
+// for (let count =1; count <= 100; count ++) {
+//     console.log("Hello, Golu!");
+// }
+
+// console.log("Loop has ended");
+
+
+//Calculate sum of 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+//     console.log(sum);
+// }
+// console.log("Final Sum =", sum); // 15 
+
+
+
+//While Loop 
+
+
+let i = 1;
+while (i <=10) {
+    console.log("i=", i);
+    i++;
+}
+
+console.log("Loop has ended");
+
