3298b5a2a323d925df675bff289e589c89d6ac64
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-05-03 20:39:50 +0530

    feat: add JavaScript array practice script and explanatory notes

---
+++ Resources/JavaScript/04_Arrays/script.js
@@ -0,0 +1,43 @@
+// let marks_student = 85;
+// let marks_student_2 = 90;
+// let marks_student_3 = 95;
+
+// console.log(marks_student, marks_student_2, marks_student_3);
+
+// let marks = {
+//     student_1 : 85,
+//     student_2 : 90,
+//     student_3 : 95
+// };                       // Object not Array [BRACES] 
+
+// let marks_arr = [85, 90, 95]; // Array [Square Brackets]
+
+// console.log(marks_arr);              // [85, 90, 95]
+// console.log(marks_arr[0]);           // 85 (Index starts from 0)
+// console.log(marks_arr[1]);           // 90
+// console.log(marks_arr[2]);           // 95
+// console.log(marks_arr[3]);           // Undefined 
+
+
+// let marks = [97, 82, 78, 91];
+
+// console.log(marks[0]);     //97
+// console.log(marks[1]);     //82 
+// console.log(marks[2]);     //78
+// console.log(marks[3]);     //91 
+// console.log(marks.length);  //4
+
+// console.log(marks[4]);     // undefined
+
+
+
+//
+
+
+
+let heros = ["shaktiman","spiderman","batman","heman"]; 
+
+console.log(heros);
+console.log(heros);
+console.log(heros.typeof);
+
