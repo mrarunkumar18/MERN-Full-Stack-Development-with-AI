17f7a5f519ef2c8eac154f5a5c4c4a92a2a75b33
mrarunkumar18 <mrarunkumar1806@gmail.com>
2026-05-03 09:03:01 +0530

    feat: add introductory resources and notes for JavaScript arrays

---
+++ Resources/JavaScript/04_Arrays/script.js
@@ -0,0 +1,14 @@
+// let marks_student1 = 95;
+// let marks_student2 = 85;
+// let marks_student3 = 75;
+// let marks_student4 = 65;
+
+// console.log(marks_student1, marks_student2, marks_student3, marks_student4);
+
+
+let marks = {
+    stu1 : 97,
+    stu2 : 82,
+    
+}  // Objects are made for this purpose! //this is object 
+
